091611a1dc19bbb5e47a08ac69dbf42ded5a3ad7
Anurag Sharma <sharmanurag0829@gmail.com>
2023-04-04 19:33:44 +0530

    intial commit

--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+interface Order{
+  TotalPrice(price:number,quntity:number):number
+}
+
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
@@ -7,4 +11,12 @@ import { Component } from '@angular/core';
 })
 export class ResumeComponent {
 
+  calculateTotal:Order={
+    TotalPrice(price, quntity) {
+      let totalPrice=12*12;
+      return totalPrice;
+    },
+
+  }
+  
 }
